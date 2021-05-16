e3e032d92106b568424b0ae7e50daf45164b6cfd
jemin <rudanijemin18@gmail.com>
2021-05-16 13:10:53 +0530

    add icone and add font

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,6 @@ void main() {
   runApp(MyApp());
 }
 
-
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -15,7 +13,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            children: <Widget> [
+            children: <Widget>[
               CircleAvatar(
                 radius: 50,
                 backgroundImage: AssetImage('images/jemin.jpg.jpeg'),
@@ -23,11 +21,67 @@ class MyApp extends StatelessWidget {
               Text(
                 'Jemin Rudani',
                 style: TextStyle(
+                  fontFamily: 'Pacifico',
                   fontSize: 30,
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
                 ),
-              )
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20,
+                  letterSpacing: 2.5,
+                  color: Colors.teal.shade200,
+                  fontWeight: FontWeight.bold,
+                ),
+
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10,horizontal: 25),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal.shade300,
+                    ),
+                    SizedBox(           //keep center
+                      width: 10,
+                    ),
+                    Text(
+                      '+91 6355933749',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10,horizontal: 25),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal.shade300,
+                    ),
+                    SizedBox(           //keep center
+                      width: 10,
+                    ),
+                    Text(
+                      'rudanijemin@gmail.com',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
@@ -35,4 +89,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
